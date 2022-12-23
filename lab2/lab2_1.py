dc1b138dfb14c9f75f3a9e990927651731ce4f67
Rinat <46104016+SRinatR@users.noreply.github.com>
2022-12-23 09:21:06 +0300

    доделал

--- lab2/lab2_1.py
+++ lab2/lab2_1.py
@@ -1,13 +1,11 @@
 a = True
 b = False
-
-print(a & b)
-print ((a & b) | b)
-print((a & b) ^ (a & b))
-print(a & b ^ (a & b) | b)
-print(b & b ^ a & (a | b | a) ^ (a | b))
+print(a and b)
+print((a and b) or b)
+print((a and b) or not (a and b))
+print(a and b or not (a or b) or b)
+print(b and b or not a and (a or b or a) or not (a or b))
 print(1 << 2)
 print(1 & 0 | 1 >> 1)
 print(1 & 0 | 1 >> 0)
 print(0b101 & 0b111 ^ 0b111 | 0b010)
-       
